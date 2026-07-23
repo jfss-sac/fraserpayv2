704ed028e3bc311dc13e206db5b5b0a848928faf
dzkchen <dzkchen@gmail.com>
2026-07-23 12:44:36 -0400

    bro ts took me too long to fix to realize its cause im not using the right dev url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import { securityHeaders } from "./src/lib/security-headers";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   async headers() {
     return securityHeaders;
   },
